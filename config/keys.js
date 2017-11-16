7e8b78ad4255de19010611e1b55571cd24650aed
danah olivetree <danah.olivetree@gmail.com>
2017-11-15 17:28:53 -0700

    Secret things

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: '767696723274-l1s6nh96rsub2ttre087s2c7j1v0h3jb.apps.googleusercontent.com',
-  googleClientSecret: 'PttVCI4U4Oi_M8w0ATqc-L6j',
-  twitterConsumerKey: 'SZ2GnUydggg32GFRJ9UE8gjp8',
-  twitterConsumerSecret: 'o1gikqVhx33Vh2bAY0CHOOwZA0FpfuIhyI2xwBFXWkcPG6Nioe',
-  mongoURI: 'mongodb://admin:password@ds163745.mlab.com:63745/adverteyesr-dev',
-  cookieKey: 'eiutnmnsldiehwqod,ld,paowuw82920wkskskkwpw[]'
-}

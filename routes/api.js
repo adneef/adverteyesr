99eddd1efbec99e2897bd32df9d2e334690ebbeb
danah olivetree <danah.olivetree@gmail.com>
2017-11-16 08:53:09 -0700

    i dont even know

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/api/search', function(req, res, next) {
+router.get('api/search', function(req, res, next) {
   console.log('*****req.query.term if we made it to api/search', req.query.term);
   console.log('*****req.body if we made it to api/search', req.body);
     console.log('*****req.params if we made it to api/search', req.params);

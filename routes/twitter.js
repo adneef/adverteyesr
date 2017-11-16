46a33d0505f6a4c4b6844f0688c69170ece1b7e3
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-16 07:41:06 -0700

    twitter api stuff

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   const Twit = require('twit')
+  // move into keys folder
   const t = new Twit({
     consumer_key: '6pqWkbCsxqFApKczNkkT7PkIz',
     consumer_secret: 'HOQctxlGDCNrnoXLBz9Uqv5bcXr9XmkEJofpv79q6u80YcshdX',
@@ -12,7 +14,7 @@ router.get('/', function(req, res, next) {
     access_token_secret: 'KOKGWEejvwL4CY8UT2PfYBMifYlYzIbLDRCVBRAzllb6w'
   })
 
-  t.get('search/tweets', { q: '#ThrowbackThursday', count: 200 }, gotData)
+  t.get('search/tweets', { q: 'ThrowbackThursday', count: 200 }, gotData)
 
   function gotData(err, data, response) {
     const locations = data.statuses.map(item => item.user.location)

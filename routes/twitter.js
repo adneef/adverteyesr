3fa0752b6f00178d7f72fd295e20e8c9a2932738
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-17 14:29:01 -0700

    got tweets organized by volume

--- routes/twitter.js
+++ routes/twitter.js
@@ -22,24 +22,47 @@ router.get('/', function(req, res, next) {
   //denver co
   t.get('trends/place', { id: 2391279 }, gotData)
 
+  // t.get('geo/search', { query: 'Boulder' }, gotData)
+
 
   function gotData(err, data, response) {
+    // console.log(data[0]);
+
+    // const trendWithNumbers = data[0].trends.filter(item => {
+    //   if (item.tweet_volume) {
+    //     console.log(item);
+    //   }
+    // })
+    //
+    // console.log(trendWithNumbers);
+
     // const locations = data.statuses.map(item => item.user.location)
     // console.log('LOCATIONS FROM API CALL: ', locations);
+
     let filteredTrends = []
-    let index = 1
     const trends = data[0].trends.map(item => item.name)
-    const trendsWithVoluem = data[0].trends.filter(item => {
+    const trendsWithVolume = data[0].trends.filter(item => {
       if (item.tweet_volume) {
         filteredTrends.push({
-          id: index,
           name: item.name,
           tweets: item.tweet_volume
         })
-        index++
       }
     })
-    console.log(filteredTrends);
+    // console.log(filteredTrends);
+    // sort objects in arrary by tweet volume
+    let sort_by = function(field, reverse, primer){
+      let key = primer ?
+        function(x) {return primer(x[field])} :
+        function(x) {return x[field]};
+      reverse = -1
+      return function (a, b) {
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+      }
+    }
+    let organizedTrends = filteredTrends.sort(sort_by('tweets'))
+    console.log(organizedTrends);
+
   }
 
 });
